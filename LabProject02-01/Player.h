64e6f27e46ba21f34ac926eaf24302ec42aed0e1
TU-PJY <viska0319@icloud.com>
2024-05-09 19:55:41 +0900

    쉴드 필요없는 변수 및 함수 제거

--- LabProject02-01/Player.h
+++ LabProject02-01/Player.h
@@ -86,15 +86,6 @@ public:
 	XMFLOAT3					m_xmf3Up = XMFLOAT3(0.0f, 1.0f, 0.0f);
 	XMFLOAT3					m_xmf3Look = XMFLOAT3(0.0f, 0.0f, 1.0f);
 
-	XMFLOAT3					m_xmf3CameraOffset = XMFLOAT3(0.0f, 0.0f, 0.0f);
-	XMFLOAT3					m_xmf3Velocity = XMFLOAT3(0.0f, 0.0f, 0.0f);
-
-	float						m_fFriction = 125.0f;
-
-	float           			m_fPitch = 0.0f;
-	float           			m_fYaw = 0.0f;
-	float           			m_fRoll = 0.0f;
-
 	int shield_hp = 255;
 
 	CCamera* m_pCamera = NULL;
@@ -103,23 +94,9 @@ public:
 	void SetPosition(float x, float y, float z);
 	void SetRotation(float x, float y, float z);
 
-	void LookAt(XMFLOAT3& xmf3LookAt, XMFLOAT3& xmf3Up);
-
-	void Move(DWORD dwDirection, float fDistance);
-	void Move(XMFLOAT3& xmf3Shift, bool bUpdateVelocity);
-	void Move(float x, float y, float z);
-
-	void Rotate(float fPitch = 0.0f, float fYaw = 0.0f, float fRoll = 0.0f);
-
-	void SetCameraOffset(XMFLOAT3& xmf3CameraOffset);
-
-	void Update(float fTimeElapsed = 0.016f);
-
+	
 	virtual void OnUpdateTransform();
 	virtual void Animate(float fElapsedTime);
 	virtual void Render(HDC hDCFrameBuffer, CCamera* pCamera);
-
-	void SetCamera(CCamera* pCamera) { m_pCamera = pCamera; }
-	CCamera* GetCamera() { return(m_pCamera); }
 };
 
